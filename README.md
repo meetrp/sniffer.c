6431de729e786a1996e9b1e0ce2f0e5b01b0b6d4
Rp <rp@meetrp.com>
2015-11-03 01:40:52 +0530

    Noop listener
    Basic plumbing to listen to traffic on RAW socket.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# sniffer.c
-Network sniffer in C. Sniff every incoming as well as outgoing Ethernet packet. Current used to capture the ARP to &amp; fro. Will extend it later.
+# packet sniffer in c
+
+Network sniffer in C. Sniff every incoming as well as outgoing Ethernet packet. Currently used to capture the ARP to &amp; fro. Will extend it later.
---
+++ src/ns_sniffer.c
@@ -0,0 +1,85 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Rp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*
+ * ns_sniffer.c
+ *
+ *  Created on		: 03-Nov-2015
+ *  Author		: rp
+ *  Date			: 12:50:24 am
+ */
+
+#include <sys/socket.h>			// socket, AF_PACKET, SOCK_RAW, htons
+#include <netinet/if_ether.h>	// ETH_P_ALL
+
+#include "ns_sniffer.h"			// includes standard header
+
+#include "ns_packet_processor.h"
+#include "ns_config.h"
+
+ns_error_t sniffer()
+{
+	ns_error_t err = ns_failure;
+	int raw_sock = -1;
+
+	unsigned char *buf;
+	struct sockaddr saddr;
+	int saddr_size;
+
+	int data_size;
+
+	raw_sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+	if (raw_sock < 0) {
+		ERR("Socket Error!");
+		err = ns_socket_failed;
+		goto out;
+	}
+
+	buf = (unsigned char*) malloc(DEFAULT_BUF_SIZE);
+	if (NULL == buf) {
+		ERR("No mem!");
+		err = ns_malloc_failed;
+		goto no_mem;
+	}
+
+	while (1) {
+		saddr_size = sizeof(saddr);
+		if ((data_size = recvfrom(raw_sock, buf, DEFAULT_BUF_SIZE, 0,
+		        &saddr, &saddr_size)) < 0) {
+			ERR("No data!");
+			err = ns_recvfrom_received_no_data;
+			goto no_data;
+		}
+
+		process_packet(buf, data_size);
+	}
+
+	err = ns_success;
+
+	no_data: free(buf);
+	no_mem: close(raw_sock);
+	out: return err;
+}
